e489e693b5d38e5b3935de95780708749119044f
hadiali-dev <haadii.ali.2007@gmail.com>
2026-04-20 13:04:28 +0300

    Keep only frontend app in website repo

--- backend/src/utils/pagination.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req) => {
-    const page = Math.max(parseInt(req.query.page) || 1, 1);
-    const limit = Math.min(parseInt(req.query.limit) || 10, 100);
-    const skip = (page - 1) * limit;
-  
-    return { page, limit, skip };
-  };
-  
